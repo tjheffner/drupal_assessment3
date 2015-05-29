18e5e6492327258f5bfbc196d17163a526e6280e
Epicodus <student@epicodus.com>
2015-05-29 08:48:49 -0700

    add installation config.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'baseassessment',
+      'username' => 'bakery',
+      'password' => 'bakery',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'gqTexwK9PJwU04KB8c6sBl_LuteiNm1WowNw38OgE-o';
 
 /**
  * Base URL (optional).
